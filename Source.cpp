4b90704c2f31ed153c5ba77bfaa7159e0221d304
Matthew Apelman <31864387+Iplaygames995@users.noreply.github.com>
2018-01-17 13:26:20 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,28 @@
-//write a program that prints a corny joke or pun to the console window :D
+#include<allegro5\allegro.h>
+#include<allegro5/allegro_image.h>
+
+int main() {
+	ALLEGRO_DISPLAY *display = NULL;
+	al_init();
+	al_init_image_addon();
+
+	display = al_create_display(700, 500);
+	//loads bitmaps from folder
+	ALLEGRO_BITMAP *a = al_load_bitmap("ninja.jpg");
+	ALLEGRO_BITMAP *b = al_load_bitmap("Fire.JPG");
+	ALLEGRO_BITMAP *c = al_load_bitmap("");
+	//png files have automatic transparency, jpgs and other file types do not
+
+
+	//render section//////////////////////////////////////////////////////////
+	al_clear_to_color(al_map_rgb(50, 10, 0));
+	
+	//the order these things are drawn in matters
+	al_draw_bitmap(a, 200, 200, 0);
+		al_draw_bitmap(b, 0, 0, 0);
+		al_flip_display();
+		al_rest(5);
+
+		al_destroy_display(display);
+
+}
